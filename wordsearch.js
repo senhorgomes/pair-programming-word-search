17a3088c8b802f4dfba5712bc670e149fc9b7f78
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-03 21:33:59 +0000

    Solution to pair programming exercise with Jessica.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
 const wordSearch = (letters, word) => { 
+    if (word === '') {
+        return false
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+    
+    let verticalArray = [];
+    for (let n = 0; n < letters[0].length; n++) {
+    let tempVerticalArray =[]
+    for(let i = 0; i < letters.length; i++) {
+      tempVerticalArray.push(letters[i][n]);
+    }
+    verticalArray.push(tempVerticalArray)
+    }
+    const verticalJoin = verticalArray.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false
+};
+
 
 module.exports = wordSearch
